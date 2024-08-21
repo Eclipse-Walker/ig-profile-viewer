d17be808fe7f27e241062d8e6102c60689f92658
EclipseWalker <phisanuratv100@hotmail.com>
2024-08-21 13:15:16 +0700

    remove unuse & refact update readme

--- background.js
+++ background.js
@@ -1,12 +1,3 @@
-chrome.contextMenus.onClicked.addListener(genericOnClick);
-
-function genericOnClick(info, tab) {
-  const { url } = tab;
-  console.log(JSON.stringify(url));
-  if (url.includes("instagram.com")) {
-    get_instagram_profile_picture(url);
-  }
-}
 chrome.runtime.onInstalled.addListener(function () {
   chrome.contextMenus.create({
     title: "IG Profile Viewer",
@@ -14,26 +5,44 @@ chrome.runtime.onInstalled.addListener(function () {
   });
 });
 
-function get_instagram_profile_picture(url) {
-  get_instagram_username(url)
-    .then(get_instagram_user_id)
-    .then(open_instagram_full_hd_photo);
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+  switch (info.menuItemId) {
+    case "parent":
+      try {
+        if (tab?.url) {
+          const { url } = tab;
+          console.log(JSON.stringify(url));
+          if (url.includes("instagram.com")) {
+            getInstagramProfilePicture(url);
+          }
+        } else {
+          console.error("Tab or URL is undefined");
+        }
+      } catch (error) {
+        console.error("error:genericOnClick: ", error);
+      }
+      break;
+    default:
+      console.error("error:contextMenus-onClicked");
+  }
+});
+
+function getInstagramProfilePicture(url) {
+  getInstagramUser(url).then(getInstagramUserId).then(openInstagramFullHDPhoto);
 }
 
-function get_instagram_username(link) {
+function getInstagramUser(link) {
   return new Promise((resolve, reject) => {
     let regex = /(?<=instagram.com\/)[A-Za-z0-9_.]+/;
     let match = link.match(regex);
     if (match) {
       let username = match[0];
       resolve(username);
-    } else {
-      reject(new Error("Username not found in URL"));
     }
   });
 }
 
-function get_instagram_user_id(username) {
+function getInstagramUserId(username) {
   return new Promise((resolve, reject) => {
     let url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`;
     modifyHeaders(
@@ -44,21 +53,6 @@ function get_instagram_user_id(username) {
       .then((res) => res.json())
       .then((out) => resolve(out.data.user.id));
   });
-}
-
-function modifiy_headers(headerStr) {
-  chrome.webRequest.onBeforeSendHeaders.addListener(
-    function (details) {
-      for (let header of details.requestHeaders) {
-        if (header.name.toLowerCase() === "user-agent") {
-          header.value = headerStr;
-        }
-      }
-      return { requestHeaders: details.requestHeaders };
-    },
-    { urls: ["https://i.instagram.com/api/v1/users/*"] },
-    ["blocking", "requestHeaders"]
-  );
 }
 
 function modifyHeaders(headerStr) {
@@ -87,7 +81,7 @@ function modifyHeaders(headerStr) {
   });
 }
 
-function open_instagram_full_hd_photo(instagram_user_id) {
+function openInstagramFullHDPhoto(instagram_user_id) {
   return new Promise((resolve, reject) => {
     modifyHeaders(
       "Mozilla/5.0 (Linux; Android 9; GM1903 Build/PKQ1.190110.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/75.0.3770.143 Mobile Safari/537.36 Instagram 103.1.0.15.119 Android (28/9; 420dpi; 1080x2260; OnePlus; GM1903; OnePlus7; qcom; sv_SE; 164094539)"
@@ -97,7 +91,6 @@ function open_instagram_full_hd_photo(instagram_user_id) {
     fetch(url)
       .then((res) => res.json())
       .then((out) => {
-        console.log(out);
         let url = out.user.hd_profile_pic_url_info.url;
         chrome.tabs.create({ url });
         resolve(url);
